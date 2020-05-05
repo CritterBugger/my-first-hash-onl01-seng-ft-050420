974a1586710b9c6c4cf1558e611833fcc2b6bb97
Luke Chen <luke.chen.2010@gmail.com>
2020-05-05 19:11:19 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,13 +1,13 @@
 def my_hash
 
-  my_hash = ["key" => "value"]
+  my_hash = {"key" => "value"}
   
 end
 
 
 def shipping_manifest
   
-  shipping_manifest = ["whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3]
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
 end
 
