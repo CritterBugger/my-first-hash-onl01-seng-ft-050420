9480b7c38672355fd53a2d5af3993a61e9937915
Luke Chen <luke.chen.2010@gmail.com>
2020-05-05 19:10:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -7,7 +7,7 @@ end
 
 def shipping_manifest
   
-  shipping_manifest = ["whale bone corsets" => 5, ""]
+  shipping_manifest = ["whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3]
 
 end
 
@@ -18,7 +18,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+  shipping_manifest["oil paintings"]
 
 end
 
